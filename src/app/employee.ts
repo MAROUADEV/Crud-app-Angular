4445ca0ed1244cf13b44cc0b61a51035f71f964b
MAROUADEV <37182094+MAROUADEV@users.noreply.github.com>
2019-08-19 19:38:00 +0100

    Update employee.ts

--- src/app/employee.ts
+++ src/app/employee.ts
@@ -1,10 +1,5 @@
 export class Employee {
-    // constructor(
-    //     public name: string,
-    //     public position: string,
-    //     public department: string,
-    //     public salary: string
-    // ){}
+ 
     name: string;
     position: string;
     department: string;
