4443bf9b847ea7a7ee9281ec18e25ed1f81e1d46
MAROUADEV <devmaroua@gmail.com>
2019-08-19 19:13:05 +0100

    initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,36 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { AppRoutingModule } from './app-routing.module';
+
+
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './ems/navbar/navbar.component';
+import { HomeComponent } from './ems/home/home.component';
+import { AddComponent } from './ems/add/add.component';
+import { EditComponent } from './ems/edit/edit.component';
+import { ShowComponent } from './ems/show/show.component';
+import { EmpService } from './emp.service';
+import { FooterComponent } from './footer/footer.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    AddComponent,
+    EditComponent,
+    ShowComponent,
+    FooterComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    AppRoutingModule
+  ],
+  providers: [EmpService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
